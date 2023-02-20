9255ab6be43e8e356636e03ba74c449bdfceaa69
PranayPatidar <112757517+PranayPatidar@users.noreply.github.com>
2023-02-20 21:58:56 +0530

    Create index.js

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+       
+        
+        <a
+         
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+         
+        </a>
+      </section>
+
+      <section className="Footer__Items">
+        <a
+         
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+         
+        </a>
+        <a
+         
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+        
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
